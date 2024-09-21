c095eac6aad9131297202c37be0257ee091abc7b
VAT26 <Vinayaktalekarvat@gmail.com>
2024-09-21 22:33:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,json,request
+from flask import Flask,json,request,jsonify
 import pymongo
 
 client = pymongo.MongoClient()
@@ -14,13 +14,27 @@ def api_msg():
     
     if request.headers['Content-Type']=='application/json':
         data=request.json
-                    
-        client['my_db']['req'].insert_one(data) 
+        print(data)
+        keys=['author','to_branch','timestamp']
+        info=extract(data,keys)
+        print(info)
+        client['my_db']['req'].insert_one(info)
               
         return json.dumps(data)
+    return jsonify({'failled sucessfully'})   
     
+#def filter(data,keys):
+#    return {key: data[key] for key in keys if key in data}
 
-
+def extract(data,keys,result=None):
+    if result is None:
+        result={}
+    for key,value in data.items():
+        if key in keys:
+            result[key]=value 
+        if isinstance(value,dict):
+            extract(value,keys,result)       
+    return result
 
 if __name__=='__main__':
     app.run(debug=True)
